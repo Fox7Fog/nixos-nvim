21ec6aee1489c71ade58f9a6f1b4a591a708a379
Fox7Fog <106879733+Fox7Fog@users.noreply.github.com>
2024-05-13 18:09:33 -0300

    First Update neovim-overlay.nix
    This is my first neovim-overlay update, still learning and testing.

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -27,10 +27,10 @@ with final.pkgs.lib; let
   # }
   all-plugins = with pkgs.vimPlugins; [
     # plugins from nixpkgs go in here.
-    # https://search.nixos.org/packages?channel=unstable&from=0&size=50&sort=relevance&type=packages&query=vimPlugins
+    https://search.nixos.org/packages?channel=unstable&from=0&size=50&sort=relevance&type=packages&query=vimPlugins
     nvim-treesitter.withAllGrammars
     luasnip # snippets | https://github.com/l3mon4d3/luasnip/
-    # nvim-cmp (autocompletion) and extensions
+    nvim-cmp (autocompletion) and extensions
     nvim-cmp # https://github.com/hrsh7th/nvim-cmp
     cmp_luasnip # snippets autocompletion extension for nvim-cmp | https://github.com/saadparwaiz1/cmp_luasnip/
     lspkind-nvim # vscode-like LSP pictograms | https://github.com/onsails/lspkind.nvim/
@@ -78,7 +78,7 @@ with final.pkgs.lib; let
     vim-repeat
     # ^ libraries that other plugins depend on
     # bleeding-edge plugins from flake inputs
-    # (mkNvimPlugin inputs.wf-nvim "wf.nvim") # (example) keymap hints | https://github.com/Cassin01/wf.nvim
+    (mkNvimPlugin inputs.wf-nvim "wf.nvim") # (example) keymap hints | https://github.com/Cassin01/wf.nvim
     # ^ bleeding-edge plugins from flake inputs
     which-key-nvim
   ];
@@ -106,8 +106,8 @@ in {
   # files you would not like to include.
   #
   # For example:
-  #
-  # nvim-pkg-no-telescope = mkNeovim {
+  
+  #nvim-pkg-no-telescope = mkNeovim {
   #   plugins = [];
   #   ignoreConfigRegexes = [
   #     "^plugin/telescope.lua"
